f3c367e4704b7cf1d84a64f76e2b3d39f4055264
Martin Said <martinsaidmmm@gmail.com>
2026-02-08 09:04:11 +1000

    refactor(FormSent): improve messaging if user email does not exist
    - Updated the FormSent component to enhance user messaging for both login and signup scenarios.
    - Added a prompt for users to check their spam folder or create an account if they do not receive the email.
    - Cleaned up code formatting for consistency.

--- src/components/ui/form-sent.tsx
+++ src/components/ui/form-sent.tsx
@@ -1,40 +1,41 @@
-import SectionWrapper from "../layout/section-wrapper";
+import SectionWrapper from '../layout/section-wrapper';
 
 interface FormSentProps {
-	type: "login" | "signup";
-	email?: string;
+  type: 'login' | 'signup';
+  email?: string;
 }
 
 export default function FormSent({ type, email }: FormSentProps) {
-	return (
-		<SectionWrapper>
-			{type === "signup" ? (
-				<>
-					<h2>We've sent you a magic link to complete your signup.</h2>
-					<h2>
-						Please check the inbox for
-						<br />
-						{email ? <>{email}</> : "your email"}
-						<br />
-						and click the link to verify your account.
-					</h2>
-				</>
-			) : (
-				<>
-					<h2>
-						We've sent you a magic link
-						<br />
-						to log in.
-					</h2>
-					<h2>
-						Please check the inbox for
-						<br />
-						{email ? <>{email}</> : "your email"}
-						<br />
-						and click the link to access your account.
-					</h2>
-				</>
-			)}
-		</SectionWrapper>
-	);
+  return (
+    <SectionWrapper>
+      {type === 'signup' ? (
+        <>
+          <h2>We've sent you a magic link to complete your signup.</h2>
+          <h2>
+            Please check the inbox for
+            <br />
+            {email ? <>{email}</> : 'your email'}
+            <br />
+            and click the link to verify your account.
+          </h2>
+          <p>Didn't get email? Check spam or create an account.</p>
+        </>
+      ) : (
+        <>
+          <h2>
+            We've sent you a magic link
+            <br />p to log in.
+          </h2>
+          <h2>
+            Please check the inbox for
+            <br />
+            {email ? <>{email}</> : 'your email'}
+            <br />
+            and click the link to access your account.
+          </h2>
+          <p>Didn't get email? Check spam or create an account.</p>
+        </>
+      )}
+    </SectionWrapper>
+  );
 }
